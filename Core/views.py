32b83eaa7d0da0907bd7fd255ce4072b2050b31d
Benson Mose <mosebemos26@gmail.com>
2026-08-27 10:31:17 +0300

    made the home site dynamic by utilizinf the context_data

--- Core/views.py
+++ Core/views.py
@@ -5,4 +5,11 @@ from django.http import HttpResponse
 # def home(request):
 #     return HttpResponse("Welcome to myGames where you find your favorite Python Games!") #this is the home page of the website. It will display a welcome message to the user.
 def home(request):
-    return render(request, 'home.html') #this is the home page of the website. It will display a welcome message to the user.
+    return render(request, 'home.html', context_data) #this is the home page of the website. It will display a welcome message to the user.
+
+# We are creating a dictionary of data to send to the HTML
+context_data = {
+    'platform_name': 'My Awesome Games',
+    'creator': 'Bemos',
+    'games_count': 0 
+}
