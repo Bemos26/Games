d3f570bcda2850daefcfff71481e6b5312c3736f
Benson Mose <mosebemos26@gmail.com>
2026-08-27 10:20:36 +0300

    added the actuall home html page, change the views so that they recognize the new html file

--- Core/views.py
+++ Core/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+# def home(request):
+#     return HttpResponse("Welcome to myGames where you find your favorite Python Games!") #this is the home page of the website. It will display a welcome message to the user.
 def home(request):
-    return HttpResponse("Welcome to myGames where you find your favorite Python Games!") #this is the home page of the website. It will display a welcome message to the user.
+    return render(request, 'home.html') #this is the home page of the website. It will display a welcome message to the user.
